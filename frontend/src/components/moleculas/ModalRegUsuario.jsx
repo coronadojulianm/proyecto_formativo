de3d554151bb103e1595087ff07bafada1a8f2bb
coronadojulianm <juliancoronadom@gmail.com>
2024-03-17 15:29:31 -0500

    Creacion de modal registrar usuario

--- frontend/src/components/moleculas/ModalRegUsuario.jsx
+++ frontend/src/components/moleculas/ModalRegUsuario.jsx
@@ -1,19 +1,44 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 function Modal() {
   const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    nombre: '',
+    identificacion: '',
+    telefono: '',
+    correo: '',
+    contraseña: '',
+    tipo_usuario: 'celador',
+    estado_usuario: 'activo'
+  });
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
 
-  const registrarUsuario = () => {
-    // Aquí puedes colocar la lógica para registrar el usuario
-    // Por ejemplo, llamar a una función para enviar los datos del usuario al servidor
-    // Una vez que el usuario está registrado, puedes cerrar el modal
-    toggleModal();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   };
-
+  
+  const registrarUsuario = async (event) => {
+    try {
+      event.preventDefault(); // Prevenir el envío predeterminado del formulario
+  
+      // Realizar la solicitud POST al servidor para registrar el usuario
+      await axios.post('http://localhost:3000/usuarios/registrar', formData);
+      alert('Usuario registrado');
+      toggleModal();
+      window.location.reload(); // Recargar la página para reflejar los cambios en la tabla
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
+    }
+  };
+  
   return (
     <>
       <button onClick={toggleModal} className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-2" type="button">
@@ -33,12 +58,47 @@ function Modal() {
               </button>
             </div>
             <div className="pt-4">
-              {/* Aquí puedes agregar el contenido del modal */}
-              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">Contenido del modal...</p>
-            </div>
-            <div className="flex justify-end pt-4">
-              <button onClick={registrarUsuario} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Registrar</button>
-              <button onClick={toggleModal} className="text-black bg-gray-200 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Cerrar</button>
+            <form onSubmit={registrarUsuario}>
+              <div className="mb-4">
+                <label htmlFor="nombre" className="block text-gray-700 font-semibold">Nombre:</label>
+                <input  type="text" id="nombre" name="nombre" value={formData.nombre} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="identificacion" className="block text-gray-700 font-semibold">Identificación:</label>
+                <input type="text" id="identificacion" name="identificacion" value={formData.identificacion} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="telefono" className="block text-gray-700 font-semibold">Teléfono:</label>
+                <input type="text" id="telefono" name="telefono" value={formData.telefono} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="correo" className="block text-gray-700 font-semibold">Correo:</label>
+                <input type="email" id="correo" name="correo" value={formData.correo} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="contraseña" className="block text-gray-700 font-semibold">Contraseña:</label>
+                <input type="password" id="contraseña" name="contraseña" value={formData.contraseña} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="tipo_usuario" className="block text-gray-700 font-semibold">Tipo de Usuario:</label>
+                <select id="tipo_usuario" name="tipo_usuario" value={formData.tipo_usuario} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required>
+                  <option value="celador">Celador</option>
+                  <option value="instructor">Instructor</option>
+                  <option value="administrador">Administrador</option>
+                </select>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="estado_usuario" className="block text-gray-700 font-semibold">Estado de Usuario:</label>
+                <select id="estado_usuario" name="estado_usuario" value={formData.estado_usuario} onChange={handleChange} className="form-input mt-1 block w-full text-black border-solid border-2 border-black rounded-md" required>
+                  <option value="activo">Activo</option>
+                  <option value="inactivo">Inactivo</option>
+                </select>
+              </div>
+              <div className="flex justify-end pt-4">
+                  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Registrar</button>
+                  <button onClick={toggleModal} className="text-black bg-gray-200 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2">Cerrar</button>
+              </div>
+            </form>
             </div>
           </div>
         </div>
