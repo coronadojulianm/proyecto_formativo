9847a2e6d5d6931ea569dda59d04366d4af16a33
coronadojulianm <juliancoronadom@gmail.com>
2024-04-23 10:04:47 -0500

    instalacion material ui

--- frontend/src/components/templates/PrestamosTemplate.jsx
+++ frontend/src/components/templates/PrestamosTemplate.jsx
@@ -4,6 +4,16 @@ import { useEffect, useState } from "react";
 import ModalReg from "../moleculas/ModalRegPrestamo";
 import ModalAct from "../moleculas/ModalActuPrestamo";
 
+// Tablas
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 export function PrestamosTemplate() {
     const [prestamos, setPrestamos] = useState([]);
 
@@ -69,6 +79,10 @@ export function PrestamosTemplate() {
         </Container>
     );
 }
+
+
+
+
 
 const Container = styled.div`
     display: flex;
